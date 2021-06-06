d675014f0591016752e3277b46934c1a7e095b99
pabotrianda <pablotrianda@gmail.com>
2021-06-06 19:11:34 -0300

    Ahora anda!

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 const path = require('path');
 
 module.exports = {
-         externals: {
+    mode: "none",
+      module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],
+  },
+    externals: {
          react: "React",
         "react-dom": "ReactDOM",
     },
-
     entry: { fileJs: '/src/index.js',
-             Boton: '/src/Boton.js'},
+             Boton: '/src/Boton.js'
+    },
     output: {
         path: path.join(__dirname + '/dist'),
-        
     },
-    module: { rules: 
-        [ { 
-            test: /\.js$/, 
-            exclude: /(node_modules|bower_components)/, 
-            use: { loader: 'babel-loader', 
-            options: { presets: ['env', 'react'] } } 
-        } ] 
-    } 
-
-
 }
 
